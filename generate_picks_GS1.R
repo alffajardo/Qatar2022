301d619b06db8b08ae63e5b7d1b2a761c775a441
alffajardo <faj.alf@gmail.com>
2022-11-19 18:19:17 -0500

    automatic README update

--- generate_picks_GS1.R
+++ generate_picks_GS1.R
@@ -112,7 +112,7 @@ mar = c(1,1,1,1),cex.main = 1,
 is.corr = F,
 type = "lower",
 hclust.method = "ward.D",tl.cex = 1,tl.srt = 45,
-number.digits = 0,cl.pos = "upper")
+number.digits = 0,cl.cex = 0.5)
 
 dev.off()
 
