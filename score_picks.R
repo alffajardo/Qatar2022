dc25498dbf2655ddf47f4866fa1862bc264d30eb
alffajardo <faj.alf@gmail.com>
2022-11-19 20:59:34 -0500

    automatic README update

--- score_picks.R
+++ score_picks.R
@@ -5,20 +5,33 @@ library(googledrive)
 library(dplyr)
 library(tidyr)
 library(readr)
+library(purrr)
 
 # read the data
 options(gargle_oauth_email = TRUE)
 drive_auth(email = TRUE)
 
 picks <- read_csv("GS1_picks.csv") %>%
-  pivot_longer(names_to = "Match",
-               values_to = "Pick",3:18)%>%
-  nest(-Match)
+  select(-Round)
+attach(picks)
 
 matches <- drive_find(pattern = "matches",type = "spreadsheet",n_max=1)$id
 
-matches <- read_sheet(matches) %>%
-  filter(complete.cases(.))
 
+matches <- read_sheet(matches) %>%
+  filter(complete.cases(.)) %>% 
+  mutate(GD = abs(Goals_Local - Goals_Visitor))
+
+result <- matches$Result
+
+picks2 <- select(picks,-c(1,2))
+
+puntos <- map_dfc(1:length(result),~result[.x] == picks2[,.x]) %>%
+  rowSums()
+
+
+scores <- data.frame(numero_participante,Nombre,puntos)
+
+write.table(scores,"scores.csv",quote = F,sep=",",row.names = F)
 
 
